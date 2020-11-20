83e45d8c29cf079088f1b2d4901ca626c09b22fa
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-11-19 21:35:26 -0300

    add register page
    finalizando a aula do dia 19_11 fazendo uma pagina de registro

--- G4car/core/views.py
+++ G4car/core/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 # Create your views here.
 
 
 def home(request):
     return render(request, 'core/index.html')
+
+
+class Registrar(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
 
 
 @login_required
